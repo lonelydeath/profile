aa662fa5425e4df05f6eead1860df0c550eec7fa
lonelydeath <34596107+lonelydeath@users.noreply.github.com>
2022-02-02 20:01:03 +0700

    vanta

---
+++ script.js
@@ -0,0 +1,35 @@
+VANTA.CLOUDS({
+    el: "#page-clouds",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    skyColor: 0xca9948,
+    cloudColor: 0xd4c9b4,
+    cloudShadowColor: 0x0,
+    speed: 0.6
+  });
+VANTA.BIRDS({
+    el: "#page-birds",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x102,
+    color1: 0xff00f0,
+    birdSize: 1.40
+  })
+  VANTA.HALO({
+    el: "#page-halo",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    amplitudeFactor: 1.60,
+    size: 1.90
+  })
